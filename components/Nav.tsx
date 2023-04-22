ba320f0afdc6386206abc0fcb153f8e2f28bac97
MingmaTenzing <pasangsherpa689@gmail.com>
2023-04-22 10:23:00 +0800

    nav bar completed

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,14 +1,93 @@
 import Image from "next/image"
 import logo from "../assests/zingshop-logo.png"
-import { BeakerIcon } from '@heroicons/react/24/solid'
+import { Bars3Icon, MagnifyingGlassIcon, ShoppingCartIcon, UserIcon } from '@heroicons/react/24/outline'
+import Link from "next/link"
 
 
 type Props = {}
 function Nav({}: Props) {
   return (
-    <div>
+    <div className="p-4 shadow-lg">
+        <div className="md:hidden">
+
+
+        <div className="flex justify-center">
         <Image src={logo} height={108} width={392} alt="zingshop logo" className=" w-[180px] object-cover" />
 
+        </div>
+        <div className="flex justify-between mt-4">
+
+
+        <div className="flex space-x-2">
+        <UserIcon className="w-8 border p-1 rounded-full border-gray-600" />
+        <ShoppingCartIcon className="w-8 border p-1 rounded-full border-gray-600" />
+        <MagnifyingGlassIcon className="w-8 border p-1 rounded-full border-gray-600" />
+        
+            
+        </div>
+        <div>
+            <Bars3Icon className="w-8 border p-1 rounded-full border-gray-600"/>
+        </div>
+        </div>
+        </div>
+
+        <div className="hidden md:flex md:items-center md:justify-between lg:px-10">
+        <div className="">
+            <Bars3Icon className="w-8 border p-1 rounded-full border-gray-600  lg:hidden"/>
+            <Image src={logo} height={108} width={392} alt="zingshop logo" className=" w-[180px] object-cover hidden lg:flex" />
+
+        </div>
+        <div>
+        <Image src={logo} height={108} width={392} alt="zingshop logo" className=" w-[180px] object-cover lg:hidden" />
+        <div className=" hidden lg:flex space-x-8 ">
+            <div  className=" group flex flex-col items-center">
+                
+            <Link href="" className=" text-sm uppercase">home</Link>
+            <span className="block w-0   group-hover:w-full transition-all ease-linear duration-200 bg-zingyellow h-0.5">
+                
+            </span>
+            </div>
+            <div  className=" group flex flex-col items-center">
+                
+            <Link href="" className=" text-sm uppercase">mens </Link>
+            <span className="block w-0   group-hover:w-full transition-all ease-linear duration-200 bg-zingyellow h-0.5">
+                
+            </span>
+            </div>
+            <div  className=" group flex flex-col items-center">
+                
+            <Link href="" className=" text-sm uppercase">womens</Link>
+            <span className="block w-0   group-hover:w-full transition-all ease-linear duration-200 bg-zingyellow h-0.5">
+                
+            </span>
+            </div>
+            <div  className=" group flex flex-col items-center">
+                
+            <Link href="" className=" text-sm uppercase">electronics</Link>
+            <span className="block w-0   group-hover:w-full transition-all ease-linear duration-200 bg-zingyellow h-0.5">
+                
+            </span>
+            </div>
+            <div  className=" group flex flex-col items-center">
+                
+            <Link href="" className=" text-sm uppercase">Jwelery</Link>
+            <span className="block w-0   group-hover:w-full transition-all ease-linear duration-200 bg-zingyellow h-0.5">
+                
+            </span>
+            </div>
+           
+        </div>
+
+            
+        </div>
+          <div className="flex space-x-2">
+        <UserIcon className="w-8 border p-1 rounded-full border-gray-600" />
+        <ShoppingCartIcon className="w-8 border p-1 rounded-full border-gray-600" />
+        <MagnifyingGlassIcon className="w-8 border p-1 rounded-full border-gray-600" />
+        
+            
+        </div>
+        </div>
     </div>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,18 @@
 import Image from 'next/image'
 import Nav from '../../components/Nav'
+import Head from 'next/head'
 
 
 
 export default function Home() {
   return (
     <main>
+      <Head>
+        <title>ZingShop</title>
+        <meta name="ZingShop" content="ZingShop is a e-commerece site where you can buy mens clothing, womens clothing, electronics and jwelery." />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/zingshop-header.png" />
+      </Head>
         <Nav />
     </main>
   )
