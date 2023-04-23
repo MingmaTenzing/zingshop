77825c6de7f4933d91a225789765014ab21b7a2d
MingmaTenzing <pasangsherpa689@gmail.com>
2023-04-23 22:48:46 +0800

    reviews section is complete

--- components/Newsletter.tsx
+++ components/Newsletter.tsx
@@ -5,7 +5,7 @@ import YouTubeIcon from '@mui/icons-material/YouTube';
 type Props = {};
 function Newsletter({}: Props) {
   return (
-    <div className="bg-[#131920] p-4 py-14 space-y-3 md:px-[200px]">
+    <div className="bg-[#131920] mt-10 p-4 py-14 space-y-3 md:px-[200px]">
       <p className="text-white font-[300] text-lg md:text-2xl ">
         Stay up-to-date on the latest trends, seasonal collections, and
         exclusive offers from ZingShop - your go-to destination for electronics,
@@ -14,12 +14,12 @@ function Newsletter({}: Props) {
       </p>
       <div className='space-x-3'>
 
-      <FacebookIcon  className='text-white' sx={{fontSize:'30px'}}/>
-      <InstagramIcon  className='text-white' sx={{fontSize:'30px'}}/>
-      <YouTubeIcon  className='text-white' sx={{fontSize:'30px'}}/>
+      <FacebookIcon  className='text-white hover:text-zingyellow transition-all ease-linear duration-200 ' sx={{fontSize:'30px'}}/>
+      <InstagramIcon  className='text-white hover:text-zingyellow transition-all ease-linear duration-200' sx={{fontSize:'30px'}}/>
+      <YouTubeIcon  className='text-white hover:text-zingyellow transition-all ease-linear duration-200' sx={{fontSize:'30px'}}/>
     </div>
     <div className='space-y-2'>
-        <h2 className='text-white text-3xl'> Subscribe to NewsLetter</h2>
+        <h2 className='text-white  text-3xl'> Subscribe to NewsLetter</h2>
         <div className='flex  items-center'>
             
         <input placeholder='enter your email address ' className=' text-white w-[300px] h-[50px] bg-transparent outline-none border p-2 border-white'></input>
