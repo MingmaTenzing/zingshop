8c71032c6c95c789a95da2c5a207e728ed7f3844
MingmaTenzing <pasangsherpa689@gmail.com>
2023-04-23 09:03:21 +0800

    partners section complete

---
+++ components/Partners.tsx
@@ -0,0 +1,37 @@
+import adidas from  "../assests/adidas.png"
+import acer from  "../assests/acer.png"
+import tommy from "../assests/tommy.png"
+import lenovo from "../assests/lenovo.png"
+import msi from "../assests/msi.png"
+import nike from "../assests/nike.png"
+import asus from "../assests/Asus-Logo.png"
+import Image from "next/image"
+type Props = {};
+function Partners({}: Props) {
+  return (
+    <div className="mt-10 p-4 bg-[#f7f7f7]">
+        <div className="space-y-3 p-3">
+
+    <h2 className=" text-center font-semibold  text-2xl text-gray-700"> No Compromises in Quality</h2>
+    <p className=" text-center font-light text-gray-600 "> We are partners with world leading brand in clothing and technology</p>
+    
+            
+        </div>
+    <div className="flex flex-wrap justify-center mt-4  lg:space-x-20">
+        <Image src={adidas} alt='adidas' width={200} height={200} className="w-[60px]  opacity-80 grayscale object-fill invert-0 m-4" />
+        <Image src={tommy} alt='adidas' width={200} height={200} className="w-[60px]  opacity-80 grayscale object-fill invert-0 m-4" />
+        <Image src={nike} alt='adidas' width={200} height={200} className="w-[60px]  opacity-80 grayscale object-fill invert-0 m-4" />
+        <Image src={acer} alt='adidas' width={200} height={200} className="w-[60px]  opacity-80 grayscale object-fill invert-0 m-4" />
+        <Image src={lenovo} alt='adidas' width={200} height={200} className="w-[60px]  opacity-80 grayscale object-fill invert-0 m-4" />
+        <Image src={msi} alt='adidas' width={200} height={200} className="w-[60px]  opacity-80 grayscale object-fill invert-0 m-4" />
+        <Image src={asus} alt='adidas' width={200} height={200} className="w-[60px]  opacity-80 grayscale object-fill invert-0 m-4" />
+
+    </div>
+    
+    </div> 
+    
+    
+    )
+    
+}
+export default Partners;
